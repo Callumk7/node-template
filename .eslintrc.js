4ce57b4549b74fdf97269e0da3014a01d589cbbc
Callum Kloos <callumkloos@gmail.com>
2023-11-15 21:32:50 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic",
+		"prettier",
+	],
+	rules: {
+		"@typescript-eslint/consistent-type-definitions": "error",
+		"@typescript-eslint/consistent-type-exports": "error",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+		"@typescript-eslint/no-misused-promises": "off",
+		"@typescript-eslint/no-floating-promises": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
+		"@typescript-eslint/no-unsafe-member-access": "off",
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+};
